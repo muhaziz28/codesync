e3c78aa1c663c0270aa7fbd347051917803022c7
kaal <haikal@kaals-MacBook-Pro.local>
2025-02-03 11:15:18 +0700

    navbar

---
+++ src/components/DasboardBtn.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { SparkleIcon } from "lucide-react";
+import { useUserRole } from "./hooks/useUserRole";
+
+function DasboardBtn() {
+  const { isCandidate, isInterviewer, isLoading } = useUserRole();
+
+  if (isCandidate || isLoading) return null;
+
+  return (
+    <Link href={"/dashboard"}>
+      <Button className="gap-2 font-medium" size={"sm"}>
+        <SparkleIcon className="size-4" />
+        Dashboard
+      </Button>
+    </Link>
+  );
+}
+
+export default DasboardBtn;
